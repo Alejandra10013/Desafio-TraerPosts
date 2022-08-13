0fb3840e16df271aa24c1dc367597951857a588e
Alejandra10013 <kalejac@gmail.com>
2022-08-13 14:08:16 -0400

    final

---
+++ script.js
@@ -0,0 +1,18 @@
+const getPosts = async () => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        let showData = document.getElementById("post-data");
+        data.forEach((post) => {
+            showData.innerHTML += `
+                <ul>
+                    <li style="font-weight: bold;">${post.title}</li>
+                    <li style="list-style-type: none;">${post.body}</li>
+                </ul>
+            `;
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
